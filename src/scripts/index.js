1304dff3f4ffe36d02549375dbae11d314ca0f21
redim <redim@redim.com>
2024-09-25 19:56:41 +0800

    creating template with ant_design beginner

---
+++ src/scripts/index.js
@@ -0,0 +1,27 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/mediastyles.css";
+const data = require("../public/data/DATA.json");
+const dataRestaurants = data.restaurants;
+
+function generateRestaurantHtml(dataRestaurants) {
+  const contentDiv = document.querySelector(".content");
+  let dataHtml = [];
+
+  dataRestaurants.forEach((data) => {
+    dataHtml.push(`
+        <div data-restId="${data.id}" class="restItemBox">
+          <div class="restName">${data.name}</div>
+          <div class="restRating">${data.rating}</div>
+          <div class="restPictureId">
+            <img src="${data.pictureId}" alt="restaurantPic">
+          </div>
+          <div class="restDescription">${data.description}</div>
+          <div class="restCity">${data.city}</div>
+        </div>`);
+  });
+
+  contentDiv.innerHTML = dataHtml.join("");
+}
+
+generateRestaurantHtml(dataRestaurants);
