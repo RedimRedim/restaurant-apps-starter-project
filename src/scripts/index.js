45829868f46b49951b5284129fe1fadc98b5463d
rud <redimredim1899@gmail.com>
2024-10-03 02:06:12 +0800

    update restaurant class

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,11 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../styles/mediastyles.css";
-const data = require("../public/data/DATA.json");
-const dataRestaurants = data.restaurants;
+import { Restaurant } from "../scripts/restaurant.js";
 
-function generateRestaurantHtml(dataRestaurants) {
-  const contentDiv = document.querySelector(".content");
-  let dataHtml = [];
+const RestaurantClass = new Restaurant();
 
-  dataRestaurants.forEach((data) => {
-    dataHtml.push(`
-        <div data-restId="${data.id}" class="restItemBox">
-          <div class="restDetails">
-          <div class="restName">${data.name}</div>
-          <div class="restRating">${data.rating}</div>
-          <div class="restCity">${data.city}</div>
-          </div>
-          <div class="restPictureId">
-            <img src="${data.pictureId}" alt="restaurantPic">
-          </div>
-          <div class="restDescription">${data.description}</div>
-        </div>`);
-  });
 
-  contentDiv.innerHTML = dataHtml.join("");
-}
 
 function showNav() {
   const showNavOnClick = document.querySelector(".showNav");
@@ -44,6 +25,6 @@ function hideSidebar() {
   });
 }
 
-generateRestaurantHtml(dataRestaurants);
+RestaurantClass.generateRestaurantHtml();
 showNav();
 hideSidebar();
---
+++ src/scripts/restaurant.js
@@ -0,0 +1,29 @@
+const data = require("../public/data/DATA.json");
+
+export class Restaurant {
+  constructor() {}
+
+  generateRestaurantHtml() {
+    const dataRestaurants = data.restaurants;
+
+    const contentDiv = document.querySelector(".content");
+    let dataHtml = [];
+
+    dataRestaurants.forEach((data) => {
+      dataHtml.push(`
+        <article data-restId="${data.id}" class="restItemBox" tabindex="0">
+          <div class="restDetails">
+          <div class="restName" tabindex="0">${data.name}</div>
+          <div class="restRating" tabindex="0">${data.rating}</div>
+          <div class="restCity" tabindex="0">${data.city}</div>
+          </div>
+          <div class="restPictureId" tabindex="0">
+            <img src="${data.pictureId}" alt="${data.name}">
+          </div>
+          <div class="restDescription" tabindex="0">${data.description}</div>
+        </article>`);
+    });
+
+    contentDiv.innerHTML = dataHtml.join("");
+  }
+}
